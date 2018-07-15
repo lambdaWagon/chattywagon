154b0f8022efff716b8fda63bcb8850691ccd7a4
Frank Faustino <nawzyah@gmail.com>
2018-07-15 11:33:53 -0700

    Testing commits

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends Component {
+  state = {};
+
   render() {
     return (
       <View style={styles.container}>
@@ -10,12 +21,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
