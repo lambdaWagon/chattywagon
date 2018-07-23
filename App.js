d5ee7e34899e045684383116435e831458fdd7b2
Frank Faustino <nawzyah@gmail.com>
2018-07-22 19:57:26 -0700

    Test react-native-maps 📍

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { getRegionForCoordinates } from './app/modules/map/utils';
+import coords from './app/modules/map/coords';
+import styles from './app/styles';
 
 export default class App extends Component {
-  state = {};
+  state = {
+    region: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0,
+      longitudeDelta: 0,
+    },
+    drivers: [],
+  };
+
+  componentWillMount() {
+    const region = getRegionForCoordinates(coords);
+    const drivers = coords.map((pos, i) => ({ position: pos, id: i }));
+    this.setState({ drivers, region });
+  }
+
+  componentDidMount() {
+    this.moveDrivers();
+  }
+
+  moveDrivers() {
+    let { drivers } = this.state;
+    setTimeout(() => {
+      drivers = drivers.map(({ id, position: { latitude, longitude } }) => ({
+        id,
+        position: { latitude: latitude + 0.00005, longitude: longitude - 0.00005 },
+      }));
+
+      this.setState({ drivers });
+      this.moveDrivers();
+    }, 1000);
+  }
 
   render() {
+    const { drivers, region } = this.state;
+    console.log(region);
     return (
-      <View style={styles.container}>
-        <Text>Hello!</Text>
-      </View>
+      <MapView style={styles.map} initialRegion={region}>
+        {drivers.map(driver => <Marker key={driver.id} coordinate={driver.position} />)}
+      </MapView>
     );
   }
 }
---
+++ app/styles/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
