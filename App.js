43a5075bed5e10387756a5edb4955128ff40e6f3
Frank Faustino <nawzyah@gmail.com>
2018-07-22 20:09:34 -0700

    Refactor App.js

--- App.js
+++ App.js
@@ -1,51 +1,5 @@
-import React, { Component } from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import React from 'react';
 
-import { getRegionForCoordinates } from './app/modules/map/utils';
-import coords from './app/modules/map/coords';
-import styles from './app/styles';
+import Map from './app/modules/map';
 
-export default class App extends Component {
-  state = {
-    region: {
-      latitude: 0,
-      longitude: 0,
-      latitudeDelta: 0,
-      longitudeDelta: 0,
-    },
-    drivers: [],
-  };
-
-  componentWillMount() {
-    const region = getRegionForCoordinates(coords);
-    const drivers = coords.map((pos, i) => ({ position: pos, id: i }));
-    this.setState({ drivers, region });
-  }
-
-  componentDidMount() {
-    this.moveDrivers();
-  }
-
-  moveDrivers() {
-    let { drivers } = this.state;
-    setTimeout(() => {
-      drivers = drivers.map(({ id, position: { latitude, longitude } }) => ({
-        id,
-        position: { latitude: latitude + 0.00005, longitude: longitude - 0.00005 },
-      }));
-
-      this.setState({ drivers });
-      this.moveDrivers();
-    }, 1000);
-  }
-
-  render() {
-    const { drivers, region } = this.state;
-    console.log(region);
-    return (
-      <MapView style={styles.map} initialRegion={region}>
-        {drivers.map(driver => <Marker key={driver.id} coordinate={driver.position} />)}
-      </MapView>
-    );
-  }
-}
+export default () => <Map />;
--- app/modules/map/index.js
+++ app/modules/map/index.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
+import { getRegionForCoordinates } from './utils';
+import coords from './coords';
+import styles from '../../styles';
+
 export default class Map extends Component {
-  /* eslint react/no-unused-state: 0 */
   state = {
-    latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    region: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0,
+      longitudeDelta: 0,
+    },
+    drivers: [],
   };
 
+  componentWillMount() {
+    const region = getRegionForCoordinates(coords);
+    const drivers = coords.map((pos, i) => ({ position: pos, id: i }));
+    this.setState({ drivers, region });
+  }
+
+  componentDidMount() {
+    this.moveDrivers();
+  }
+
+  moveDrivers() {
+    let { drivers } = this.state;
+    setTimeout(() => {
+      drivers = drivers.map(({ id, position: { latitude, longitude } }) => ({
+        id,
+        position: { latitude: latitude + 0.00005, longitude: longitude - 0.00005 },
+      }));
+
+      this.setState({ drivers });
+      this.moveDrivers();
+    }, 1000);
+  }
+
   render() {
-    const { region } = this.state;
+    const { drivers, region } = this.state;
     return (
-      <MapView region={region}>
-        <Marker draggable />
+      <MapView style={styles.map} initialRegion={region}>
+        {drivers.map(driver => <Marker key={driver.id} coordinate={driver.position} />)}
       </MapView>
     );
   }
