a1a3e16d032e15f0921e4d0863b691e29ab0692e
Frank Faustino <nawzyah@gmail.com>
2018-07-24 16:56:48 -0700

    Refactor code Facebook login now writes to Firebase DB Add prop-types

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,14 +1,63 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  /* General */
   container: {
     flex: 1,
-    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  /* Map */
   map: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  /* Login */
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#AAADBA',
+    marginTop: 10,
+    width: 180,
+    textAlign: 'center',
+    opacity: 0.9,
+  },
+
+  /* LoginForm */
+  formContainer: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'rgba(255,130, 0,0.2)',
+    marginBottom: 10,
+    color: 'black',
+    paddingHorizontal: 10,
+    textAlign: 'center',
+    width: 300,
+    borderRadius: 50,
+    borderWidth: 2,
+    borderColor: '#ffcd99',
+  },
+
+  buttonContainer: {
+    marginBottom: 30,
+    borderRadius: 50,
+    height: 40,
+    backgroundColor: 'rgba(255,130, 0,0.2)',
+    borderWidth: 2,
+    borderColor: '#ffcd99',
+  },
+
+  buttonText: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontWeight: '800',
   },
 });
