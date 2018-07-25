b644f383e83c8aff7a8ea9f8025df6756369e8e8
Frank Faustino <nawzyah@gmail.com>
2018-07-24 23:20:06 -0700

    Add SplashScreen.js Add splash.png Refactor Login.js

---
+++ app/modules/auth/SplashScreen.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Button, ImageBackground, View } from 'react-native';
+import { LinearGradient } from 'expo';
+import PropTypes from 'prop-types';
+
+import { auth } from '../../config/firebase';
+import styles from '../../styles';
+
+export default class Login extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  componentDidMount() {
+    const {
+      navigation: { navigate },
+    } = this.props;
+    auth.onAuthStateChanged(user => {
+      if (user) navigate('Map');
+    });
+  }
+
+  render() {
+    const {
+      navigation: { navigate },
+    } = this.props;
+    return (
+      <ImageBackground
+        source={require('../../../assets/splash.png')}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <LinearGradient
+          colors={[
+            'rgba(196,244,255,0.8)',
+            'rgba(196,244,255,0.8)',
+            'rgba(232,134,60,0.8)',
+            'rgba(232,134,60,0.8)',
+          ]}
+          style={styles.gradient}
+        >
+          <View style={styles.logoContainer}>
+            <Button title="Get Started →" onPress={() => navigate('Login')} />
+          </View>
+        </LinearGradient>
+      </ImageBackground>
+    );
+  }
+}
--- app/modules/auth/index.js
+++ app/modules/auth/index.js
@@ -1,2 +1,3 @@
 export { default as Login } from './Login';
 export { default as LoginForm } from './LoginForm';
+export { default as SplashScreen } from './SplashScreen';
