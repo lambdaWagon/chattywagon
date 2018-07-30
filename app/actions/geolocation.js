f927f4c6f6aaaad36dd30a3eaa4826ea95f48ee9
Frank Faustino <nawzyah@gmail.com>
2018-07-30 02:03:09 -0700

    Add redux-saga Update driver location now works 👍 Move directions to own component

--- app/actions/geolocation.js
+++ app/actions/geolocation.js
@@ -1,13 +1,20 @@
 import { Dimensions } from 'react-native'
+import { buffers, eventChannel } from 'redux-saga'
+import { take, call, put } from 'redux-saga/effects'
 
 import { database, geoFire } from '../config/firebase'
 import { getRegionFromPoint } from '../modules/map/utils'
-import * as ActionType from '../constants'
+import * as types from '../constants'
 
 const { width, height } = Dimensions.get('window')
 const aspectRatio = width / height
 
-const handleErrors = error => ({ type: ActionType.ERROR, error })
+const geoQuery = geoFire.query({
+  center: [37.786279, -122.406456],
+  radius: 10
+})
+
+const handleErrors = error => ({ type: types.ERROR, error })
 
 export const getLocation = () => dispatch => {
   navigator.geolocation.getCurrentPosition(
@@ -15,7 +22,7 @@ export const getLocation = () => dispatch => {
       // console.log('📍 CURRENT LOCATION', latitude, longitude)
       const currentLocation = { latitude, longitude }
       const region = getRegionFromPoint(latitude, longitude, aspectRatio)
-      dispatch({ type: ActionType.GET_LOCATION, currentLocation, region })
+      dispatch({ type: types.GET_LOCATION, currentLocation, region })
     },
     error => {
       console.log(error)
@@ -25,39 +32,23 @@ export const getLocation = () => dispatch => {
   )
 }
 
-export const getDrivers = () => async dispatch => {
-  try {
-    const snapshot = await database.ref('driverLocations').once('value')
-    if (snapshot) {
-      const drivers = Object.entries(snapshot.val()).map(([k, v]) => ({
-        id: k,
-        latitude: v.l[0],
-        longitude: v.l[1]
-      }))
-      // console.log('🚕 DRIVERS', drivers)
-      dispatch({ type: ActionType.GET_DRIVERS, drivers })
-    }
-  } catch (error) {
-    console.log(error)
-    dispatch(handleErrors(error))
-  }
-}
-
-export const updateDrivers = () => {
-  // TO-DO: redux-saga
-  // console.log(getState().geolocation.currentLocation)
-  // const query = geoFire.query({
-  //   center: [0, 0],
-  //   radius: 10
-  // })
-  // await query.on('key_moved', (key, location, distance) => {
-  //   dispatch({ type: 'DRIVER_UPDATED', key, location })
-  //   console.log(`${key} moved within query to ${location} ${distance} km from center`)
-  // })
-  // await query.on('key_exited', (key, location, distance) => {
-  //   console.log(`${key} exited query to ${location} ${distance} km from center`)
-  // })
-}
+// export const getDrivers = () => async dispatch => {
+//   try {
+//     const snapshot = await database.ref('driverLocations').once('value')
+//     if (snapshot) {
+//       const drivers = Object.entries(snapshot.val()).map(([k, v]) => ({
+//         id: k,
+//         latitude: v.l[0],
+//         longitude: v.l[1]
+//       }))
+//       // console.log('🚕 DRIVERS', drivers)
+//       dispatch({ type: types.GET_DRIVERS, drivers })
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     dispatch(handleErrors(error))
+//   }
+// }
 
 export const setPickupLocation = ({
   nativeEvent: {
@@ -65,7 +56,7 @@ export const setPickupLocation = ({
   }
 }) => dispatch => {
   const pickupLocation = getRegionFromPoint(latitude, longitude, 300)
-  dispatch({ type: ActionType.SET_PICKUP, pickupLocation })
+  dispatch({ type: types.SET_PICKUP, pickupLocation })
 }
 
 export const setDestination = ({
@@ -75,7 +66,85 @@ export const setDestination = ({
 }) => dispatch => {
   // const destination = getRegionFromPoint(latitude, longitude, 300)
   const destination = { latitude, longitude }
-  dispatch({ type: ActionType.SET_DESTINATION, destination })
+  dispatch({ type: types.SET_DESTINATION, destination })
 }
 
 export const setAddress = address => ({ type: 'SET_ADDRESS', address })
+
+const addDriver = ({ key, location, distance }) => {
+  console.log('🚕', key, location, distance)
+  const driver = { key, latitude: location[0], longitude: location[1], distance }
+  return { type: 'ADD_DRIVER_SUCCESS', driver }
+}
+
+const updateDriver = ({ key, location, distance }) => {
+  console.log('🚕', key, location, distance)
+  const driver = { key, latitude: location[0], longitude: location[1], distance }
+  return { type: 'FETCH_DRIVER_SUCCESS', driver }
+}
+
+const removeDriver = key => {
+  console.log('🔑', key)
+  return { type: 'REMOVE_DRIVER_SUCCESS', key }
+}
+
+function subscribe(buffer) {
+  return eventChannel(emit => {
+    geoQuery.on('key_entered', (key, location, distance) =>
+      emit(addDriver({ key, location, distance }))
+    )
+    geoQuery.on('key_moved', (key, location, distance) =>
+      emit(updateDriver({ key, location, distance }))
+    )
+    geoQuery.on('key_exited', key => emit(removeDriver(key)))
+    return () => geoQuery.cancel()
+  }, buffer)
+}
+
+export function* watchDrivers() {
+  const buffer = buffers.expanding()
+  const channel = yield call(subscribe, buffer)
+
+  while (true) {
+    const action = yield take(channel)
+    yield put(action)
+  }
+}
+
+// export const updateDrivers = () => {
+// TO-DO: redux-saga
+// console.log(getState().geolocation.currentLocation)
+// const query = geoFire.query({
+//   center: [0, 0],
+//   radius: 10
+// })
+// await query.on('key_moved', (key, location, distance) => {
+//   dispatch({ type: 'DRIVER_UPDATED', key, location })
+//   console.log(`${key} moved within query to ${location} ${distance} km from center`)
+// })
+// await query.on('key_exited', (key, location, distance) => {
+//   console.log(`${key} exited query to ${location} ${distance} km from center`)
+// })
+
+// async componentDidMount() {
+//   const query = geoFire.query({
+//     center: [37.786279, -122.406456],
+//     radius: 10
+//   })
+//   await query.on('key_moved', (key, location, distance) => {
+//     this.onDriverChange(key, location)
+//     console.log(`${key} moved within query to ${location} ${distance} km from center`)
+//   })
+//   await query.on('key_exited', (key, location, distance) => {
+//     console.log(`${key} exited query to ${location} ${distance} km from center`)
+//   })
+// }
+
+// onDriverChange = (key, location) => {
+//   const { drivers } = this.props
+
+//   const updatedDrivers = drivers.map(driver => (driver.id === key
+//     ? { id: key, latitude: location[0], longitude: location[1] }
+//     : driver))
+//   this.setState({ updatedDrivers })
+// }
