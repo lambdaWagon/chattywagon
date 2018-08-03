ae1c9d5499dbfb71b765d999347da9a75b896578
Frank Faustino <nawzyah@gmail.com>
2018-08-03 03:05:43 -0700

    Refactor Directions.js Add 'implicit-arrow-linebreak' to .eslintrc

--- app/modules/map/Directions.js
+++ app/modules/map/Directions.js
@@ -3,38 +3,45 @@ import { Marker } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import * as actions from '../../actions'
 import { config } from '../../config/firebase'
 
-const Directions = props => {
-  const { coords, destination, currentLocation, destinationSet, fitToCoords } = props
+const Directions = ({ coords, destination, currentLocation, destinationSet, fitToCoords }) => (
+  <Fragment>
+    {destinationSet && (
+      <MapViewDirections
+        origin={currentLocation}
+        destination={destination}
+        apikey={config.googleMapsApiKey}
+        strokeWidth={3}
+        strokeColor="black"
+        onStart={params =>
+          console.log(`Started routing between "${params.origin}" and "${params.destination}"`)
+        }
+        onReady={fitToCoords}
+      />
+    )}
+    {coords && (
+      <Marker
+        pinColor="yellow"
+        title="Destination"
+        draggable
+        coordinate={coords[coords.length - 1]}
+      />
+    )}
+  </Fragment>
+)
 
-  return (
-    <Fragment>
-      {destinationSet && (
-        <MapViewDirections
-          origin={currentLocation}
-          destination={destination}
-          apikey={config.googleMapsApiKey}
-          strokeWidth={3}
-          strokeColor="black"
-          onStart={params =>
-            console.log(`Started routing between "${params.origin}" and "${params.destination}"`)
-          }
-          onReady={fitToCoords}
-        />
-      )}
-      {coords && (
-        <Marker
-          pinColor="yellow"
-          title="Destination"
-          draggable
-          coordinate={coords[coords.length - 1]}
-        />
-      )}
-    </Fragment>
-  )
+Directions.defaultProps = { coords: null }
+
+Directions.propTypes = {
+  coords: PropTypes.arrayOf(PropTypes.number),
+  currentLocation: PropTypes.object.isRequired,
+  destination: PropTypes.string.isRequired,
+  destinationSet: PropTypes.bool.isRequired,
+  fitToCoords: PropTypes.func.isRequired
 }
 
 const mapStateToProps = ({ geolocation: { currentLocation, destination, destinationSet } }) => ({
