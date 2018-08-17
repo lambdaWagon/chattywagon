080130490520c4e9c572e1dcef951ffa1a0e6bad
Frank Faustino <nawzyah@gmail.com>
2018-08-17 03:31:01 -0700

    Refactor SocialAccount.js

--- src/components/auth/SocialAccount.js
+++ src/components/auth/SocialAccount.js
@@ -1,105 +1,45 @@
 import React from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp
-} from 'react-native-responsive-screen'
+import { View, StyleSheet } from 'react-native'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { LinearGradient } from 'expo'
-import Icon from 'react-native-vector-icons/FontAwesome'
 import PropTypes from 'prop-types'
 
 import * as actions from '../../actions'
+import { Button, Gradient, SocialButton } from '../shared'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  linearGradient: {
-    position: 'absolute',
-    height: hp('100%'),
-    width: wp('100%')
-  },
-  middleContainer: {
-    height: hp('40%'),
-    width: wp('80%')
-  },
   socialIconsContainer: {
-    paddingTop: hp('5%'),
-    paddingBottom: hp('5%'),
-    paddingLeft: wp('5%'),
+    width: 294,
+    padding: 25,
     backgroundColor: 'white'
   },
-  socialTouch: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: wp('2%')
-  },
-  socialIcons: {
-    marginRight: wp('5%')
-  },
-  socialText: {
-    fontSize: wp('5%')
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  button: {
     justifyContent: 'center',
-    padding: wp('5%'),
-    backgroundColor: 'black',
-    marginBottom: hp('2.25%'),
-    height: hp('9.5%')
-  },
-  text: {
-    color: 'white',
-    paddingLeft: wp('1%'),
-    fontSize: wp('4.5%'),
-    fontWeight: 'bold'
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0
   }
 })
 
 const SocialAccount = ({ navigation: { navigate }, signInWithFacebook, signUserOut }) => (
-  <View style={styles.container}>
-    <LinearGradient colors={['#7fbeff', '#fff', '#ff8200']} style={styles.linearGradient} />
-
-    <View style={styles.middleContainer}>
-      <View style={styles.socialIconsContainer}>
-        <TouchableOpacity style={styles.socialTouch} onPress={() => navigate('SocialLogin')}>
-          <Icon style={styles.socialIcons} name="google" size={wp('7.5%')} color="black" />
-          <Text style={styles.socialText}>Google</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.socialTouch} onPress={() => navigate('SocialLogin')}>
-          <Icon style={styles.socialIcons} name="twitter" size={wp('7.5%')} color="black" />
-          <Text style={styles.socialText}>Twitter</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.socialTouch} onPress={signInWithFacebook}>
-          <Icon style={styles.socialIcons} name="facebook-square" size={wp('7.5%')} color="black" />
-          <Text style={styles.socialText}>Facebook</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.socialTouch} onPress={signUserOut}>
-          <Icon style={styles.socialIcons} name="envelope" size={wp('7.5%')} color="black" />
-          <Text style={styles.socialText}>Email</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <Text style={styles.text}>CHOOSE AN ACCOUNT</Text>
-      </View>
+  <Gradient>
+    <View style={styles.socialIconsContainer}>
+      <SocialButton type="Google" navigate={() => navigate('SocialLogin')} />
+      <SocialButton type="Twitter" navigate={signUserOut} />
+      <SocialButton type="Facebook" navigate={signInWithFacebook} />
+      <SocialButton type="Email" navigate={() => navigate('SocialLogin')} />
     </View>
-  </View>
+    <Button disabled icon={false} style={styles.button}>
+      CHOOSE AN ACCOUNT
+    </Button>
+  </Gradient>
 )
 
 SocialAccount.propTypes = {
-  signInWithFacebook: PropTypes.func.isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired
-  }).isRequired
+  }).isRequired,
+  signInWithFacebook: PropTypes.func.isRequired,
+  signUserOut: PropTypes.func.isRequired
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch)
